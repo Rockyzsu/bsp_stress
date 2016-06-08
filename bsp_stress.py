7c3c3aee635cecefca23a5ecc76755eda536c636
xda-linux <xda-linux@qq.com>
2016-06-08 16:17:42 +0800

    initial

--- bsp_stress.py
+++ bsp_stress.py
@@ -1,10 +1,49 @@
 #-*-coding=utf-8-*-
 import subprocess
-forced_recovery_cmd=['adb','reboot','forced-recovery']
-flash_cmd=['./tegraflash.py','--bct','bct_p2894.bct', '--bl','cboot.bin.signed','--cfg','flash_t210_darcy_android_sdmmc.xml','--odmdata','0x1296000','--bldtb', 'tegra210-darcy-p2894-0050-a04-00.dtb','--chip', '0x21', '--applet','rcm_1_signed.rcm','--nct', './nct.bin', '--cmd', 'read', 'EKS EKS_bak.bin','read','FCT fct_bak.bin', 'read', 'NCT','nct_bak.bin','secureflash', 'write', 'FCT' ,'fct_bak.bin', 'write', 'NCT' ,'nct_bak.bin', 'write,' 'EKS','EKS_bak.bin; reboot;"','--securedev']
-result=subprocess.Popen(forced_recovery_cmd,stdout=subprocess.PIPE)
-result.wait()
-print "Going to flash"
-result=subprocess.Popen(flash_cmd,stdout=subprocess.PIPE)
-result.wait()
-print result.stdout.read()
+import time
+import re
+def nvflash():
+    wait_device_cmd=['adb','wait-for-device']
+
+    result=subprocess.Popen(wait_device_cmd,stdout=subprocess.PIPE)
+    result.wait()
+
+
+    forced_recovery_cmd=['adb','reboot','forced-recovery']
+    result=subprocess.Popen(forced_recovery_cmd,stdout=subprocess.PIPE)
+    result.wait()
+
+    print "Going to flash"
+
+
+    cmd=['./shell_script.sh']
+    result=subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True)
+    result.wait()
+    print result.stdout.read()
+
+    result=subprocess.Popen(wait_device_cmd,stdout=subprocess.PIPE)
+    result.wait()
+
+    dmesg_cmd=['adb','shell','dmesg']
+    result=subprocess.Popen(dmesg_cmd,stdout=subprocess.PIPE)
+    result.wait()
+    dmesg_string=result.stdout.read()
+    if re.search(r'nvhdcp: link integrity check passed',dmesg_cmd):
+        print "Passed"
+        return True
+    else:
+        print "Failed"
+        return False
+
+
+def main():
+    pass_count=0
+    total_count=50
+    for i in range(total_count):
+        time.sleep(5)
+        print "*"*20
+        if nvflash():
+            pass_count=pass_count+1
+
+        print "Pass count %d/%d" %(pass_count,total_count)
+

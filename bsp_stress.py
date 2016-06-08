1c1a4f53136efcff3e1b3a6c73bdc9cf9fb4bafc
xda-linux <xda-linux@qq.com>
2016-06-08 18:57:38 +0800

    update

--- bsp_stress.py
+++ bsp_stress.py
@@ -2,31 +2,50 @@
 import subprocess
 import time
 import re
-def nvflash():
-    wait_device_cmd=['adb','wait-for-device']
-
-    result=subprocess.Popen(wait_device_cmd,stdout=subprocess.PIPE)
-    result.wait()
+def nvflash_fun():
 
 
+    #wait_device_cmd=['adb','wait-for-device']
+    wait_device_cmd=["adb shell dmesg"]
+
+    result=subprocess.Popen(wait_device_cmd,shell=True,stdout=subprocess.PIPE)
+    #result.wait()
+    stdout1=result.communicate()
+    print "Now print result"
+    #result.wait()
+    #print result.stdout.read()
+    print type(result)
+    for i in result.stdout.readlines():
+        print i
+    #print result.stdout.readlines()
+    print type(stdout1)
+
+    #print result
+
+
+    '''
     forced_recovery_cmd=['adb','reboot','forced-recovery']
-    result=subprocess.Popen(forced_recovery_cmd,stdout=subprocess.PIPE)
-    result.wait()
+
+    result1=subprocess.Popen(forced_recovery_cmd,stdout=subprocess.PIPE)
+    result1.wait()
 
     print "Going to flash"
 
-
+    #
     cmd=['./shell_script.sh']
-    result=subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True)
-    result.wait()
-    print result.stdout.read()
+    result2=subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True)
+    #result.wait()
+    #print result.stdout.read()
+    print "waiting on sleep"
+    time.sleep(500)
 
-    result=subprocess.Popen(wait_device_cmd,stdout=subprocess.PIPE)
-    result.wait()
+    print "Flashing done"
+    result3=subprocess.Popen(wait_device_cmd,stdout=subprocess.PIPE)
+    result3.wait()
 
     dmesg_cmd=['adb','shell','dmesg']
-    result=subprocess.Popen(dmesg_cmd,stdout=subprocess.PIPE)
-    result.wait()
+    result4=subprocess.Popen(dmesg_cmd,stdout=subprocess.PIPE)
+    result4.wait()
     dmesg_string=result.stdout.read()
     if re.search(r'nvhdcp: link integrity check passed',dmesg_cmd):
         print "Passed"
@@ -34,16 +53,23 @@ def nvflash():
     else:
         print "Failed"
         return False
-
+    '''
 
 def main():
     pass_count=0
-    total_count=50
+    total_count=1
+
     for i in range(total_count):
-        time.sleep(5)
+        #time.sleep(5)
         print "*"*20
+        print i
+        nvflash_fun()
+        '''
         if nvflash():
             pass_count=pass_count+1
 
         print "Pass count %d/%d" %(pass_count,total_count)
+        '''
 
+if __name__=="__main__":
+    main()
